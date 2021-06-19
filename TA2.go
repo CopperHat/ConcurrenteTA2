9be1b36b2854bb5863c903aa1d2dc7a29544b6ef
CopperHat <copperhat@outlook.com>
2021-06-19 01:14:14 -0500

    Update TA2.go

--- TA2.go
+++ TA2.go
@@ -10,58 +10,9 @@ import (
 	"net/http"
 )
 
-type Option struct {
-	Value, Id, Text string
-	Selected        bool
-}
-
 const HTML = `
 <!DOCTYPE html>
 <html lang="en">
-     <head>
-        <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>selected attribute</title>
-<link rel="stylesheet" type="text/css" href="fondo.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    </head>
-
-
-
-    <body style=' margin: 0;
-    padding: 0;
-	background: url("https://www.entornointeligente.com/wp-content/uploads/2020/02/entornointeligente_roberto_pocaterra_pocaterra_buenos_aires_lo_que_tienes_que_saber_sobre_las_nuevas_cifras_del_coronavirus.jpg");    
-	background-size: cover;
-	background-position: center;
-	background-repeat: no-repeat;
-    font-family: sans-serif;'>
-
-<div class="w3-container w3-lobster">                                                 
-        <h1 style='font-family: "Lobster", serif;  position: absolute; top: 50px; left: 670px;'>Bienvenido al Analizador Covid</h1>
-</div>
-
-<div class="w3-container w3-lobster">                                                 
-		<h1 style='font-family: "Lobster", serif; color: #05687; position: absolute; top: 110px; left: 100px;'>Por favor</h1>
-		<h1 style='font-family: "Lobster", serif; color: #05687; position: absolute; top: 170px; left: 30px;'>tomate tu tiempo y </h1>
-		<h1 style='font-family: "Lobster", serif; color: #05687; position: absolute; top: 230px; left: 100px;'>selecciona </h1>
-		<h1 style='font-family: "Lobster", serif; color: #05687; position: absolute; top: 300px; left: 30px;'>la opcion correcta </h1>
-		<h1 style='font-family: "Lobster", serif; color: #05687; position: absolute; top: 370px; left: 42px;'>para asegurar </h1>
-		<h1 style='font-family: "Lobster", serif; color: #05687; position: absolute; top: 440px; left: 35px;'>un buen resultado </h1>
-	
-</div>
-
-
-<div class="flip-card style = left: 30px;'">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="https://i.ytimg.com/vi/mA1qCnk4Lg4/hqdefault.jpg" alt="" width="330" height="600">
-    </div>
-  </div>
-</div>
-
         <form method="GET">                                                                                                   
 			<label>UBIGEO:</label>
 			<select id="UBIGEO" name="UBIGEO">
@@ -133,7 +84,6 @@ const HTML = `
                    background-color: #009C8C; border: none; border-radius: 15px; box-shadow: 0 9px #999;' type="submit" value="Analizar" align="center" name = "submit">
            </div>
         </form>
-
     </body>
 </html>
 `
@@ -141,12 +91,6 @@ const HTML = `
 var placesPageTmpl *template.Template = template.Must(template.New("	").Parse(HTML))
 
 const localAddr = "192.168.0.9:8000"
-
-const (
-	cnum = iota
-	opContagiado
-	opNoContagiado
-)
 
 var chInfo chan map[string]int
 
@@ -171,11 +115,10 @@ type estadoRegistro struct {
 
 var addrs = []string{
 	"192.168.0.27:8000",
-	"192.168.0.28:8000"
-}
+	"192.168.0.28:8000"}
 
 func main() {
-	url := "https://raw.githubusercontent.com/mledoze/countries/master/dist/countries.csv"
+	url := "https://github.com/CopperHat/ConcurrenteTA2/blob/main/Muestra_2da_vuelta_Version_ONPE_0.csv"
 	data, err := readCSVFromUrl(url)
 	if err != nil {
 		panic(err)
@@ -195,7 +138,7 @@ func main() {
 	}
 	fmt.Print(addrs)
 	fmt.Println()
-	http.HandleFunc("/", name)
+	http.HandleFunc("/", register)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -216,8 +159,7 @@ func readCSVFromUrl(url string) ([][]string, error) {
 	return data, nil
 }
 
-
-func name(w http.ResponseWriter, r *http.Request) {
+func register(w http.ResponseWriter, r *http.Request) {
 	var registro = Registro{}
 
 	registro.UBIGEO = r.FormValue("UBIGEO")
@@ -240,10 +182,6 @@ func handle(conn net.Conn) {
 		log.Println("Can't decode from", conn.RemoteAddr())
 	} else {
 		fmt.Println(msg)
-		switch msg.Code {
-		case cnum:
-			concensus(conn, msg)
-		}
 	}
 }
 
@@ -253,6 +191,7 @@ func concensus(conn net.Conn, msg estadoRegistro) {
 
 	go func() { chInfo <- info }()
 }
+
 func send(remoteAddr string, msg estadoRegistro) {
 	if conn, err := net.Dial("tcp", remoteAddr); err != nil {
 		log.Println("Can't dail", remoteAddr)
